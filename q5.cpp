ca2fce7e323225d9f3b359e166569f2f4a299a1a
rqiao <volpato30@gmail.com>
2018-10-02 10:47:42 -0400

    fix

--- q5.cpp
+++ q5.cpp
@@ -10,24 +10,22 @@
 using std::default_random_engine;
 using std::uniform_real_distribution;
 
-default_random_engine generator;
-
 double phi(double x) {
     return 2 * x / (1 + 1.0 / 3 * pow(x, 4));
 }
 
 double computeVariance(std::vector<double> x_array) {
     double mean = 0, variance = 0;
-    unsigned long n = x_array.size();
+    unsigned long number_of_experiments = x_array.size();
     for (double &x : x_array) {
         mean += x;
     }
-    mean = mean / n;
+    mean = mean / number_of_experiments;
     for (double &x : x_array) {
         variance += pow(x - mean, 2);
     }
 
-    variance = variance / (n - 1);
+    variance = variance / (number_of_experiments - 1);
     return variance;
 }
 
@@ -52,10 +50,13 @@ ExpResult _getResult(std::vector<double> r_unif_array) {
 
 ExpResult antitheticMethod(unsigned long n) {
     std::vector<double> r_unif_array(n);
+    //mercene twister generator
+    std::mt19937 rng;
+    rng.seed(std::random_device()());
     uniform_real_distribution<double> rGen(0.0, 1.0);
     double temp;
     for (int i = 0; i < int(n / 2); i++) {
-        temp = rGen(generator);
+        temp = rGen(rng);
         r_unif_array[2 * i] = 2 * temp;
         r_unif_array[2 * i + 1] = 2 * (1 - temp);
     }
@@ -64,17 +65,18 @@ ExpResult antitheticMethod(unsigned long n) {
 
 ExpResult crudeMethod(unsigned long n) {
     std::vector<double> r_unif_array(n);
-    ;
+    std::mt19937 rng;
+    rng.seed(std::random_device()());
     uniform_real_distribution<double> rGen(0.0, 1.0);
     for (double &x : r_unif_array) {
-        x = 2 * rGen(generator);
+        x = 2 * rGen(rng);
     }
     return _getResult(r_unif_array);
 };
 
 void q5() {
-    unsigned long n = 1400;
-    unsigned long number_of_experiments = 1000;
+    unsigned long n = 100;
+    unsigned long number_of_experiments = 10000;
     std::vector<double> crudeMC(number_of_experiments);
     std::vector<double> antitheticMC(number_of_experiments);
 
@@ -83,12 +85,18 @@ void q5() {
         antitheticMC[i] = antitheticMethod(n).mean;
     }
 
+    double c_mean = 0, a_mean = 0;
+
     std::cout << "q5:" << std::endl;
-    std::cout << "number of experiments:\t" << n << std::endl;
+
+    std::cout << c_mean / number_of_experiments << std::endl;
+    std::cout << a_mean / number_of_experiments << std::endl;
+
+    std::cout << "number of experiments:\t" << number_of_experiments << std::endl;
     std::cout << "Crude MC variance:\t" << computeVariance(crudeMC) << std::endl;
     std::cout << "Antithetic MC variance:\t" << computeVariance(antitheticMC) << std::endl;
 
-    n = 10000;
+    n = 1000000;
     auto result1 = crudeMethod(n);
     auto result2 = antitheticMethod(n);
     std::cout << "sample size:\t" << n << std::endl;
